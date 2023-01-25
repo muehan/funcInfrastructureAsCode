77a385abe67ea9c9d8c8a5781460d7354eef0d7d
André <tdc@gmx.ch>
2023-01-25 18:00:44 +0000

    change function

--- AddResourceGroup.cs
+++ AddResourceGroup.cs
@@ -14,22 +14,28 @@ namespace funcInfrastructureAsCode
     {
         [FunctionName("AddResourceGroup")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string name = data?.name;
+            string location = data?.location;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            var resource = new ResourceGroup {
+                name = name,
+                location = location
+            };
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(JsonConvert.SerializeObject(resource));
         }
+    }
+
+    public struct ResourceGroup
+    {
+        public string name { get; set; }
+        public string location { get; set; }
     }
 }
