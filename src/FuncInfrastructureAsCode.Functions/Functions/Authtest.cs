3ca0f42f7b952f76efa2e8be4bc1bfdde9440a6c
André <tdc@gmx.ch>
2023-04-20 09:44:56 +0200

    add auth function for test

---
+++ src/FuncInfrastructureAsCode.Functions/Functions/Authtest.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Security.Claims;
+
+namespace FuncInfrastructureAsCode.Functions
+{
+    public static class Authtest
+    {
+        [FunctionName("Authtest")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var sb = new StringBuilder();
+            var identity = req.HttpContext?.User?.Identity as ClaimsIdentity;
+            sb.AppendLine($"IsAuthenticated: {identity?.IsAuthenticated}");
+            sb.AppendLine($"Identity name: {identity?.Name}");
+            sb.AppendLine($"AuthenticationType: {identity?.AuthenticationType}");
+            foreach (var claim in identity?.Claims)
+            {
+                sb.AppendLine($"Claim: {claim.Type} : {claim.Value}");
+            }
+            return new OkObjectResult(sb.ToString());
+        }
+    }
+}
